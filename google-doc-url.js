5e75f226acbf50829ce5d48838c7bc526ff6797c
billymcconvell <43562809+billymcconvell@users.noreply.github.com>
2018-10-26 22:00:22 +1100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Zlxb1uDVKsACL4RpVISLNue1_nnmjktVA_gGdeDO9NM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NlobJwFlWzGmAa7Y6DHw8rf9zPWK0t9-LzqFBzZdDDg/edit#gid=0';
